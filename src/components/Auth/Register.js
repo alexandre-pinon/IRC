382f190afecda5d56ba167eda197abf93483b5c6
Keisay <kinsay.soundcloud@gmail.com>
2021-01-05 23:24:39 +0100

    Register Page

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -4,11 +4,61 @@ import { Link } from 'react-router-dom';
 
 
 class Register extends React.Component {
-    state = {}
+    state = {
+        username: "",
+        email: "",
+        password: "",
+        passwordConfirmation: "",
+        errors: []
+    };
 
-    handleChange = () => {}
+    isFormValid = () => {
+        let errors = [];
+        let error;
+
+        if (this.isFormEmpty(this.state)) {
+            error = { message: 'Fill in all fields' };
+            this.setState({ errors: errors.concat(error) });
+            return false;
+        } else if (!this.isPasswordValid()) {
+            error = { message: 'Password is invalid' };
+            this.setState({ errors: errors.concat(error) });
+            return false;
+        } else {
+            // form valid
+            return true;
+        }
+    };
+
+    isFormEmpty = ({ username, email, password, passwordConfirmation }) => {
+        return !username.length || !email.length || !password.length ||
+        !passwordConfirmation.length;
+    }
+
+    isPasswordValid = ({ password, passwordConfirmation }) => {
+        if (password.length < 6 || passwordConfirmation.length < 6) {
+            return false;
+        } else if (password !== passwordConfirmation) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+    handleSubmit = event => {
+        if (this.isFormValid()) {
+        event.preventDefault();
+
+        }
+    };
 
     render() {
+        const { username, email, password, passwordConfirmation } = this.state;
+
         return (
             <Grid textAlign="center" verticalAlign="middle" className="app">
                 <Grid.Column style={{ maxWidth: 450 }}>
@@ -16,21 +66,20 @@ class Register extends React.Component {
                         <Icon name="chat" color="blue" />
                         Register for PlooV4
                     </Header>
-                    <Form size="large">
+                    <Form onSubmit={this.handleSubmit} size="large">
                         <Segment stacked>
                             <Form.Input fluid name="username" icon="user" iconPosition="left"
-                            placeholder="Username" onChange={this.handleChange} type="text" />
+                            placeholder="Username" onChange={this.handleChange} value={username} type="text" />
 
                             <Form.Input fluid name="email" icon="mail" iconPosition="left"
-                            placeholder="Email" onChange={this.handleChange} type="email" />
+                            placeholder="Email" onChange={this.handleChange} value={email} type="email" />
 
                             <Form.Input fluid name="password" icon="lock" iconPosition="left"
-                            placeholder="Password" onChange={this.handleChange} type="password" />
+                            placeholder="Password" onChange={this.handleChange} value={password} type="password" />
 
                             <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left"
-                            placeholder="Password Confirmation" onChange={this.handleChange} type="password" />
+                            placeholder="Password Confirmation" onChange={this.handleChange} value={passwordConfirmation} type="password" />
                         
-                            <Button color="blue" fluid size="large">Submit</Button>
                             <Button color="blue" fluid size="large">Submit</Button>
 
                         </Segment>
