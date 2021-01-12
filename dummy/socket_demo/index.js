b329ce35ca3a804bbf2bbc97a5d1371a4cdb786e
Keisay <kinsay.soundcloud@gmail.com>
2021-01-12 21:19:59 +0100

    Socket Test

---
+++ dummy/socket_demo/index.js
@@ -0,0 +1,21 @@
+const { isObject } = require('util');
+
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
